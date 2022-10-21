4a608866cec433b5256d7da7f0e6cb448f09406e
Abrar Mahi <amahi2001@gmail.com>
2022-10-21 00:27:43 -0400

    view bugs boilerplate

--- lib/pages/edit_projects/widgets/view_bugs.dart
+++ lib/pages/edit_projects/widgets/view_bugs.dart
@@ -16,29 +16,109 @@ class ViewBugOverlay extends StatefulWidget {
 
 class _ViewBugOverlayState extends State<ViewBugOverlay> {
 
-  late Stream<DocumentSnapshot> bugStream;
+  late Stream<QuerySnapshot> bugStream;
 
   @override
   void initState() {
     super.initState();
     print(widget.stackID);
-    bugStream = widget.stackCollection.doc(widget.stackID).snapshots();
+    bugStream = widget.stackCollection.doc(widget.stackID).collection('Bugs').snapshots();
   }
 
   @override
   Widget build(BuildContext context) {
-      return StreamBuilder(builder: (context, snapshot) {
-        
-        if (snapshot.hasData) {
-          return Container(
-            child: Text(snapshot.data.toString()),
-          );
-        } else {
-          return Container(
-            child: Text("No data"),
-          );
+      return StreamBuilder<QuerySnapshot>(
+      stream: bugStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          //todo some text that says no data
+          // String error = snapshot.error.toString();
+          // print(error);
+          // return Text(error, style: const TextStyle(color: Colors.white));
         }
-      });
-      
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading", style: TextStyle(color: Colors.white));
+        }
+
+        return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: snapshot.data!.size,
+            itemBuilder: (context, index) {
+              QueryDocumentSnapshot<Object?> bug =
+                  snapshot.data!.docs[index];
+              return Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: InkWell(
+                    onTap: () => {
+                      //todo navigate to edit bug/ view bug page
+                    },
+                    child: Card(
+                      elevation: 10,
+                      color: const Color.fromARGB(255, 22, 66, 97),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          side: const BorderSide(
+                              color: Color.fromARGB(255, 146, 153, 192),
+                              width: 1)),
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              fit: FlexFit.tight,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 15,
+                                          top: 6,
+                                          right: 15,
+                                          bottom: 3),
+                                          //todo bug info
+                                      child: Text("some info",
+                                          style: const TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 241, 240, 244),
+                                              fontSize: 20,
+                                              wordSpacing: 3))),
+                                  Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 15, right: 15, bottom: 6),
+                                          //todo bug info
+                                      child: Text(
+                                          "bug info",
+                                          style: const TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 230, 229, 232),
+                                              fontSize: 12,
+                                              wordSpacing: 5))),
+                                ],
+                              ),
+                            ),
+                            // Delete project button
+                            IconButton(
+                              icon: const Icon(
+                                Icons.delete_sweep_rounded,
+                                size: 30,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {
+                                //todo show "are you sure" dialog
+                              },
+                            ),
+                          ]),
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    );
+
   }
 }
