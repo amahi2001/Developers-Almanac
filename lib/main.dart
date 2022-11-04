27cf19b0941cd7dbff371a9f3c757631276b46a2
Abrar <amahi2001@gmail.com>
2022-11-04 17:32:55 -0400

    **ignore** -i

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 //auth
-import 'auth.dart';
-import 'pages/home_page.dart';
 import 'pages/login_page.dart';
-
 
 DateTime today = DateTime(
     DateTime.now().year,
@@ -21,7 +17,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  ).then((value) => runApp(MyApp()));
+  ).then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
