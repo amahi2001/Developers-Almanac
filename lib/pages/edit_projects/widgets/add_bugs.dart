a03242e016d42635c86d6f497b33ef1361bf77e3
Abrar <amahi2001@gmail.com>
2022-11-06 11:49:53 -0500

    fixed add_bug form renderviewport error

--- lib/pages/edit_projects/widgets/add_bugs.dart
+++ lib/pages/edit_projects/widgets/add_bugs.dart
@@ -68,167 +68,165 @@ class _AddBugPopUpState extends State<AddBugPopUp> {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: AlertDialog(
-        title: const Padding(
-            padding: EdgeInsets.all(3.0),
-            child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text("Add Bug",
-                    style: TextStyle(fontWeight: FontWeight.bold)))),
-        content: Form(
-            key: _addBugKey,
-            child: Container(
-              width: 300,
-              height: formHeight,
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(.0),
-                    child: TextFormField(
-                      controller: _bugNameController,
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter Bug Name:',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Bug Name';
-                        }
-                        return null;
-                      },
+    return AlertDialog(
+      title: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text("Add Bug",
+                  style: TextStyle(fontWeight: FontWeight.bold)))),
+      content: Form(
+          key: _addBugKey,
+          child: SizedBox(
+            width: 300,
+            height: formHeight,
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(.0),
+                  child: TextFormField(
+                    controller: _bugNameController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter Bug Name:',
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Bug Name';
+                      }
+                      return null;
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(.0),
-                    child: TextFormField(
-                      controller: _bugDescriptionController,
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter Bug Description:',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Bug Description';
-                        }
-                        return null;
-                      },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(.0),
+                  child: TextFormField(
+                    controller: _bugDescriptionController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter Bug Description:',
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Bug Description';
+                      }
+                      return null;
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(.0),
-                    child: TextFormField(
-                      controller: _bugErrorOutputController,
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter Error Output:',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Error Output';
-                        }
-                        return null;
-                      },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(.0),
+                  child: TextFormField(
+                    controller: _bugErrorOutputController,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter Error Output:',
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Error Output';
+                      }
+                      return null;
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: CheckboxListTile(
-                      title: const Text("Bug Solved"),
-                      value: _bugSolved,
-                      onChanged: (val) {
-                        setState(() {
-                          _bugSolved = val!;
-                          formHeight = formHeight == 220 ? 340 : 220;
-                          callback();
-                        });
-                      },
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: CheckboxListTile(
+                    title: const Text("Bug Solved"),
+                    value: _bugSolved,
+                    onChanged: (val) {
+                      setState(() {
+                        _bugSolved = val!;
+                        formHeight = formHeight == 220 ? 340 : 220;
+                        callback();
+                      });
+                    },
                   ),
-                  Visibility(
-                    visible: _bugSolved,
-                    child: Padding(
-                        padding: const EdgeInsets.all(.0),
-                        child: DropdownSearch<String>(
-                          popupProps: const PopupProps.menu(
-                            showSearchBox: true,
-                            showSelectedItems: true,
-                            // disabledItemFn: (String s) => s.startsWith('I'),
-                          ),
-                          items: langs,
-                          dropdownDecoratorProps: const DropDownDecoratorProps(
-                            dropdownSearchDecoration: InputDecoration(
-                              labelText: "Programming Language",
-                              hintText: "country in menu mode",
-                            ),
-                          ),
-                          onChanged: (val) {
-                            setState(() => _bugLanguageController = val!);
-                          },
-                          selectedItem: langs.first,
-                        )),
-                  ),
-                  Visibility(
-                    visible: _bugSolved,
-                    child: Padding(
+                ),
+                Visibility(
+                  visible: _bugSolved,
+                  child: Padding(
                       padding: const EdgeInsets.all(.0),
-                      child: TextFormField(
-                        keyboardType: TextInputType.multiline,
-                        minLines: 3,
-                        maxLines: null,
-                        controller: _bugSolutionsController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Enter Solution:',
+                      child: DropdownSearch<String>(
+                        popupProps: const PopupProps.menu(
+                          showSearchBox: true,
+                          showSelectedItems: true,
+                          // disabledItemFn: (String s) => s.startsWith('I'),
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter Solution';
-                          }
-                          return null;
+                        items: langs,
+                        dropdownDecoratorProps: const DropDownDecoratorProps(
+                          dropdownSearchDecoration: InputDecoration(
+                            labelText: "Programming Language",
+                            hintText: "country in menu mode",
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() => _bugLanguageController = val!);
                         },
+                        selectedItem: langs.first,
+                      )),
+                ),
+                Visibility(
+                  visible: _bugSolved,
+                  child: Padding(
+                    padding: const EdgeInsets.all(.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 3,
+                      maxLines: null,
+                      controller: _bugSolutionsController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter Solution:',
                       ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Solution';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ],
-              ),
-            )),
-        actions: [
-          TextButton(
-            child: const Text('Cancel'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          TextButton(
-            child: const Text('Add Bug'),
-            onPressed: () {
-              if (_addBugKey.currentState!.validate()) {
-                List<Solution> addArray = [
-                  Solution(
-                      solution: _bugSolutionsController.text,
-                      language: _bugLanguageController)
-                ];
-                widget.project_query_doc
-                    .collection("Stack")
-                    .doc(widget.stack_id)
-                    .collection("Bug")
-                    .add({
-                  'bug_name': _bugNameController.text,
-                  'bug_description': _bugDescriptionController.text,
-                  'error_output': _bugErrorOutputController.text,
-                  'solution': _bugSolutionsController.text,
-                  'is_solved': _bugSolved,
-                  'created_at': today,
-                }).then((value) {
-                  print("Bug Added");
-                  Navigator.pop(context);
-                }).catchError((error) => print("Failed to add bug: $error"));
-              }
-            },
-          ),
-        ],
-      ),
+                ),
+              ],
+            ),
+          )),
+      actions: [
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        TextButton(
+          child: const Text('Add Bug'),
+          onPressed: () {
+            if (_addBugKey.currentState!.validate()) {
+              List<Solution> addArray = [
+                Solution(
+                    solution: _bugSolutionsController.text,
+                    language: _bugLanguageController)
+              ];
+              widget.project_query_doc
+                  .collection("Stack")
+                  .doc(widget.stack_id)
+                  .collection("Bug")
+                  .add({
+                'bug_name': _bugNameController.text,
+                'bug_description': _bugDescriptionController.text,
+                'error_output': _bugErrorOutputController.text,
+                'solution': _bugSolutionsController.text,
+                'is_solved': _bugSolved,
+                'created_at': today,
+              }).then((value) {
+                print("Bug Added");
+                Navigator.pop(context);
+              }).catchError((error) => print("Failed to add bug: $error"));
+            }
+          },
+        ),
+      ],
     );
   }
 }
