b55ac9b90a37bba8de564b5c0aba69a6d53c8158
Nafisa Tanta <nafisantanta@gmail.com>
2022-12-17 23:31:50 -0500

    Emptying view bugs preview (in edit projects) when deleting stacks

--- lib/pages/edit_projects/widgets/delete_stack.dart
+++ lib/pages/edit_projects/widgets/delete_stack.dart
@@ -1,11 +1,17 @@
+// import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:devs_almanac/pages/edit_projects/edit_project.dart';
+import 'package:devs_almanac/pages/edit_projects/widgets/view_bugs.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../helpers/helper.dart';
 import '../../home_page/home_page.dart';
 
 /// This widget lets the user delete a stack given a stack ID
 class DeleteStackPopup extends StatefulWidget {
+  final Function() callback;
   final Function() notifyParent;
   final id;
   final DocumentReference<Object?> query_doc;
@@ -15,6 +21,7 @@ class DeleteStackPopup extends StatefulWidget {
       {super.key,
       required this.query_doc,
       required this.id,
+      required this.callback,
       required this.notifyParent,
       required this.stackName});
 
@@ -40,7 +47,13 @@ class _DeleteStackPopupState extends State<DeleteStackPopup> {
             stackDoc = widget.query_doc.collection('Stack').doc(widget.id);
             stackNum = await getBugsInStack(stackDoc);
             projectBugs -= stackNum;
+            widget.callback();
             widget.notifyParent();
+            selectedID = "";
+            stackName = "Placeholder";
+            stackType = "Placeholder";
+            stackID = "";
+            bugCount = null;
             Navigator.pop(context);
           },
           child: const Text('Delete'),
